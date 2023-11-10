d3d909620e26a81750ae9cce841717e127a96043
Xegtor <rohakkansal04@gmail.com>
2023-11-10 20:52:18 +0530

    changes

--- src/main/java/com/approject/stickhero/Controller.java
+++ src/main/java/com/approject/stickhero/Controller.java
@@ -3,7 +3,7 @@ package com.approject.stickhero;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
